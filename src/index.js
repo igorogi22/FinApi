df4e08f8e2a9a1f45bb724d6d0388c7cbeb03e34
Igor Vinicius <igorvinicius.oli@gmail.com>
2021-09-10 22:58:21 -0300

    feat: added withdraw operation

--- src/index.js
+++ src/index.js
@@ -21,6 +21,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
     return next();
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === 'credit') {
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount
+        }
+    }, 0);
+
+    return balance;
+}
+
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
 
@@ -59,6 +71,27 @@ app.post("/account/deposit", (request, response) => {
         created_at: new Date(),
         type: "credit"
     }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+})
+
+app.post("/account/withdraw", (request, response) => {
+    const { customer } = request;
+    const { amount } = request.body;
+
+    const balance = getBalance(customer.statement);
+
+    if(amount > balance) {
+        return response.status(400).json({ error: "Insufficient funds!!" })
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    };
 
     customer.statement.push(statementOperation);
 
