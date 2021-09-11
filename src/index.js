8fcf89e9f83013033fab79c7a15b67659f68e4da
Igor Vinicius <igorvinicius.oli@gmail.com>
2021-09-10 21:29:55 -0300

    feat: cpf validation created so there are no duplicates

--- src/index.js
+++ src/index.js
@@ -10,13 +10,16 @@ const customers = [];
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
 
+    const costumerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
 
-    const id = uuidv4();
+    if(costumerAlreadyExists) {
+        return response.status(400).json({ error: "Customer already exists!" });
+    }
 
     customers.push({
         cpf,
         name,
-        id,
+        id: uuidv4(),
         statement: []
     })
 
